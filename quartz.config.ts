6801f41563a37eab76fea81ec178239796c2cf54
Lee Seul <love.and.seul@gmail.com>
2025-07-24 21:51:14 +0900

    Quartz sync: Jul 24, 2025, 9:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "seulwithlove.github.io/seulseul",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
